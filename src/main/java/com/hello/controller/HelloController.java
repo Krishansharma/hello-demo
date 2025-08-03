2b7b0cda722987dc96e29be158e6bba1da460a24
Krishan kumar sharma <krishan@omniesolutions.com>
2025-08-03 12:08:55 +0530

    add name endpoint

--- src/main/java/com/hello/controller/HelloController.java
+++ src/main/java/com/hello/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.hello.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,11 @@ public class HelloController {
 				+ " applications and services across a global network of Microsoft-managed data centers."
 				+ " Essentially, Azure provides the infrastructure, platform, and software needed to "
 				+ "run applications and services in the cloud, eliminating the need for on-premises hardware. ";
+	}
+	
+	@GetMapping("/addName/{name}")
+	public String getName(@PathVariable String name) {
+		return name;
 	}
 
 }
