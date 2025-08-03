bab2b9b2ded80e2107f5d979229d0499d8f7767e
Krishan kumar sharma <krishan@omniesolutions.com>
2025-08-03 11:36:27 +0530

    create new endpoint

--- src/main/java/com/hello/controller/HelloController.java
+++ src/main/java/com/hello/controller/HelloController.java
@@ -12,5 +12,15 @@ public class HelloController {
 	public String getMessage() {
 		return "This is my first project to deploy azure";
 	}
+	
+	@GetMapping("/msg")
+	public String getTextMsg() {
+		return "Microsoft Azure is a cloud computing platform and service that offers a "
+				+ "wide array of services including computing, storage, networking, analytics,"
+				+ " and artificial intelligence. It allows users to build, deploy, and manage"
+				+ " applications and services across a global network of Microsoft-managed data centers."
+				+ " Essentially, Azure provides the infrastructure, platform, and software needed to "
+				+ "run applications and services in the cloud, eliminating the need for on-premises hardware. ";
+	}
 
 }
